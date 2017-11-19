d26005a294cfbaa3d28b5617a5b4db76326e2bbf
AlyssonG <alyssongomes@id.uff.br>
2017-11-19 14:49:57 -0200

    Criação dos métodos inclui no membroappservice

--- src/main/java/servico/MembroAppService.java
+++ src/main/java/servico/MembroAppService.java
@@ -1,4 +1,36 @@
 package servico;
 
+import dao.MembroDAO;
+import dao.TimeDAO;
+import excecao.MembroNaoEncontradoException;
+import excecao.ObjetoNaoEncontradoException;
+import excecao.TimeNaoEncontradoException;
+import modelo.Membro;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
 public class MembroAppService {
+    @Autowired
+    private TimeDAO timeDAO;
+
+    @Autowired
+    private MembroDAO membroDAO;
+
+    @Transactional
+    public long inclui(Membro umMembro) throws TimeNaoEncontradoException {
+        //incluir regra de membros em um só time
+        //membroDAO.find(membro)
+        //se não achar, inclui
+        Membro membro = membroDAO.inclui(umMembro);
+        return membro.getId();
+    }
+
+    @Transactional
+    public void exclui(Membro umMembro) throws MembroNaoEncontradoException {
+        try {
+            membroDAO.exclui(umMembro);
+        } catch (ObjetoNaoEncontradoException e) {
+            throw new MembroNaoEncontradoException("Membro não encontrado.");
+        }
+    }
 }
