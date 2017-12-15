05488e8cb7881f9af1044a3841ed062670bcc6e3
AlyssonG <alyssongomes@id.uff.br>
2017-12-15 20:41:24 -0200

    Mesclagem do aspecto de verificação do banco com a de permissões

--- src/main/java/aspecto/AspectoAround.java
+++ src/main/java/aspecto/AspectoAround.java
@@ -1,14 +1,19 @@
 package aspecto;
 
+import Permissao.PermissaoManager;
+import excecao.ExecucaoDeMetodoSemARespectivaPermissaoException;
 import excecao.ViolacaoDeConstraintDesconhecidaException;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.reflections.Reflections;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -36,6 +41,9 @@ public class AspectoAround {
     @Around("traduzExcecaoAround()")
     public Object traduzExcecaoAround(ProceedingJoinPoint joinPoint) throws Throwable {
         try {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Method method = signature.getMethod();
+            checkPermission(method.getDeclaredAnnotations());
             return joinPoint.proceed();
         } catch (org.springframework.dao.DataAccessException e) {
             Throwable t = e;
@@ -58,6 +66,14 @@ public class AspectoAround {
             } else {
                 throw e;
             }
+        }
+    }
+
+    private void checkPermission(Annotation[] annotations) throws Throwable{
+        PermissaoManager permissaoManager = PermissaoManager.getInstance();
+        for(Annotation annotation : annotations) {
+            if (annotation.toString().contains("Role") && !permissaoManager.hasPermission(annotation.toString()))
+                throw new ExecucaoDeMetodoSemARespectivaPermissaoException("Você não tem permissão para efetuar esta operação");
         }
     }
 }
