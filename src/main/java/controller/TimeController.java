e2cd1a9c660c7b80df7c85908813d2ecfcfb44bd
AlyssonG <alyssongomes@id.uff.br>
2017-12-16 16:44:52 -0200

    Criação da janela de time

--- src/main/java/controller/TimeController.java
+++ src/main/java/controller/TimeController.java
@@ -3,10 +3,79 @@ package controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import modelo.Time;
+import servico.MembroAppService;
+import servico.ServiceSingleton;
+import servico.TimeAppService;
 
 public class TimeController {
     @FXML
     Button btnNovoTime, btnCadastrarTime, btnEditarTime, btnAlterarTime, btnBuscarTime;
     @FXML
     TextField nomeTime, ligaTime;
+    private MembroAppService membroService;
+    private TimeAppService timeService;
+
+    @FXML
+    public void initialize() {
+        membroService = ServiceSingleton.getInstance().getMembroService();
+        timeService = ServiceSingleton.getInstance().getTimeService();
+        turnOffFields();
+    }
+
+    @FXML
+    private void novoTime() {
+        btnNovoTime.setDisable(true);
+        turnOnFields();
+    }
+
+    @FXML
+    private void cadastrarTime() {
+        btnNovoTime.setDisable(false);
+        btnCadastrarTime.setDisable(true);
+        Time time = new Time(nomeTime.getText(), ligaTime.getText());
+        timeService.inclui(time);
+    }
+
+    @FXML
+    private void editarTime() {
+        btnNovoTime.setDisable(false);
+        btnEditarTime.setDisable(true);
+        turnOnFields();
+    }
+
+    @FXML
+    private void alterarTime() {
+        btnNovoTime.setDisable(false);
+        turnOffFields();
+        cleanFields();
+    }
+
+    @FXML
+    private void buscarTime() {
+        btnNovoTime.setDisable(false);
+
+    }
+
+    @FXML
+    private void checkFields() {
+        if (nomeTime.getText().length() == 0 && ligaTime.getText().length() == 0)
+            btnCadastrarTime.setDisable(true);
+        btnCadastrarTime.setDisable(false);
+    }
+
+    private void turnOnFields() {
+        nomeTime.setDisable(false);
+        ligaTime.setDisable(false);
+    }
+
+    private void turnOffFields() {
+        nomeTime.setDisable(true);
+        ligaTime.setDisable(true);
+    }
+
+    private void cleanFields() {
+        nomeTime.setText("");
+        ligaTime.setText("");
+    }
 }
