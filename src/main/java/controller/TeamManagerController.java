71d114e0606ed9045d2e283fb6ead7c1ba893e94
AlyssonG <alyssongomes@id.uff.br>
2017-12-16 12:56:30 -0200

    Criação do controller de time

--- src/main/java/controller/TeamManagerController.java
+++ src/main/java/controller/TeamManagerController.java
@@ -1,3 +1,5 @@
+package controller;
+
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -11,65 +13,11 @@ import javax.swing.*;
 public class TeamManagerController {
 
     @FXML
-    Pane rootPane,membroPane, timePane;
-
-    @FXML
-    Button btnNovoMembro, btnCadastrarMembro, btnEditarMembro, btnAlterarMembro, btnBuscarMembro;
-    @FXML
-    TextField nomeMembro, posicaoMembro, dataMembro;
-
-    @FXML
-    Button btnNovoTime, btnCadastrarTime, btnEditarTime, btnAlterarTime, btnBuscarTime;
-    @FXML
-    TextField nomeTime, ligaTime;
+    Pane rootPane, membroPane, timePane;
 
     public void initialize() {
         disablePane(membroPane);
         disablePane(timePane);
-    }
-
-    @FXML
-    private void novoMembro() {
-        nomeMembro.setEditable(true);
-        posicaoMembro.setEditable(true);
-        dataMembro.setEditable(true);
-    }
-
-    @FXML
-    private void cadastrarMembro() {
-        nomeMembro.setEditable(false);
-        posicaoMembro.setEditable(false);
-        dataMembro.setEditable(false);
-        //chamar BD com informações
-    }
-
-    @FXML
-    private void editarMembro() {
-
-    }
-
-    @FXML
-    private void alterarMembro() {
-
-    }
-
-    @FXML
-    private void buscarMembro() {
-
-    }
-
-    @FXML
-    private void checkDataMembro() {
-        if (dataMembro.getText().length() < 9) {
-            btnCadastrarMembro.setDisable(true);
-        } else {
-            btnCadastrarMembro.setDisable(false);
-        }
-    }
-
-    private void enablePane(Pane pane) {
-        pane.setDisable(false);
-        pane.setVisible(true);
     }
 
     private void disablePane(Pane pane) {
@@ -84,6 +32,5 @@ public class TeamManagerController {
         Scene scene = new Scene(membroPane,450,300);
         newStage.setScene(scene);
         newStage.show();
-        //enablePane(membroPane);
     }
 }
---
+++ src/main/java/controller/TimeController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class TimeController {
+    @FXML
+    Button btnNovoTime, btnCadastrarTime, btnEditarTime, btnAlterarTime, btnBuscarTime;
+    @FXML
+    TextField nomeTime, ligaTime;
+}
