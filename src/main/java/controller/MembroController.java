9cb26d7a73c1cb3d1baa5a3637f312c796a72bee
AlyssonG <alyssongomes@id.uff.br>
2017-12-16 16:11:51 -0200

    Configuração da seleção de time

--- src/main/java/controller/MembroController.java
+++ src/main/java/controller/MembroController.java
@@ -1,28 +1,76 @@
 package controller;
 
+import excecao.MembroJaExistenteException;
+import excecao.TimeNaoEncontradoException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import modelo.Membro;
+import modelo.Time;
+import servico.MembroAppService;
+import servico.ServiceSingleton;
+import servico.TimeAppService;
+import util.Util;
 
-public class MembroController{
+import java.util.Set;
+
+public class MembroController {
+    MembroAppService membroService;
+    TimeAppService timeService;
+
     @FXML
     Button btnNovoMembro, btnCadastrarMembro, btnEditarMembro, btnAlterarMembro, btnBuscarMembro;
     @FXML
     TextField nomeMembro, posicaoMembro, dataMembro;
+    @FXML
+    ComboBox timesMembro;
 
     @FXML
-    private void novoMembro() {
-        nomeMembro.setEditable(true);
-        posicaoMembro.setEditable(true);
-        dataMembro.setEditable(true);
+    public void initialize() {
+        membroService = ServiceSingleton.getInstance().getMembroService();
+        timeService = ServiceSingleton.getInstance().getTimeService();
     }
 
     @FXML
-    private void cadastrarMembro() {
-        nomeMembro.setEditable(false);
-        posicaoMembro.setEditable(false);
-        dataMembro.setEditable(false);
-        //chamar BD com informações
+    private void novoMembro() {
+        nomeMembro.setDisable(false);
+        posicaoMembro.setDisable(false);
+        dataMembro.setDisable(false);
+
+        timesMembro.setItems(getTeamsToComboBox());
+        timesMembro.setDisable(false);
+    }
+
+    @FXML
+    private void cadastrarMembro() throws TimeNaoEncontradoException, MembroJaExistenteException {
+        nomeMembro.setDisable(true);
+        posicaoMembro.setEditable(true);
+        dataMembro.setEditable(true);
+        timesMembro.setEditable(true);
+
+        Membro m = new Membro(nomeMembro.getText(),
+                posicaoMembro.getText(),
+                Util.strToCalendar(dataMembro.getText()),
+                getTime((String)timesMembro.getValue()));
+
+        membroService.inclui(m);
+        nomeMembro.setText("");
+        posicaoMembro.setText("");
+        dataMembro.setText("");
+        timesMembro.setValue("");
+    }
+
+    private Time getTime(String text) {
+        Set<Time> ts = timeService.recuperaTimesEMembros();
+        for(Time t : ts){
+            if(t.getNome().equals(text)){
+                return t;
+            }
+        }
+        return null;
     }
 
     @FXML
@@ -47,5 +95,14 @@ public class MembroController{
         } else {
             btnCadastrarMembro.setDisable(false);
         }
+    }
+
+    private ObservableList<String> getTeamsToComboBox() {
+        Set<Time> ts = timeService.recuperaTimesEMembros();
+        ObservableList<String> timesNome = FXCollections.observableArrayList();
+        for (Time t : ts) {
+            timesNome.add(t.getNome());
+        }
+        return timesNome;
     }
 }
