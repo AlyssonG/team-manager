61895e3383ce62bd0d2f28f4d98c40c1190ac846
AlyssonG <alyssongomes@id.uff.br>
2017-12-17 12:04:30 -0200

    Preenchendo dados da busca

--- src/main/java/controller/BuscaTimeController.java
+++ src/main/java/controller/BuscaTimeController.java
@@ -1,13 +1,19 @@
 package controller;
 
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.util.Callback;
 import modelo.Time;
+import servico.ServiceSingleton;
+import servico.TimeAppService;
+
+import java.util.Set;
 
 public class BuscaTimeController {
     @FXML
@@ -19,27 +25,68 @@ public class BuscaTimeController {
     @FXML
     TableView tableTime;
 
-    @FXML
-    TableColumn nomeColumn, ligaColumn, selectColumn;
+    TableColumn nomeColumn, ligaColumn, selecionarColumn;
+
+    private TimeAppService timeService;
 
     ObservableList<Time> times;
 
     @FXML
     void initialize() {
-        nomeColumn.setCellFactory(new PropertyValueFactory<Time, String>("nome"));
-        ligaColumn.setCellFactory(new PropertyValueFactory<Time, String>("liga"));
-        selectColumn.setCellFactory((Callback<TableColumn.CellDataFeatures<Time, Boolean>, ObservableValue<Boolean>>) param ->
-                new SimpleBooleanProperty((param.getValue() != null)));
-        times = getTimes();
-        tableTime.setItems(times);
-    }
+        nomeColumn = new TableColumn("Nome");
+        nomeColumn.setCellValueFactory(new PropertyValueFactory<TimeRow, String>("nome"));
 
-    private ObservableList<Time> getTimes() {
-        return null;
+        ligaColumn = new TableColumn("Liga");
+        ligaColumn.setCellValueFactory(new PropertyValueFactory<TimeRow, String>("liga"));
+
+        selecionarColumn = new TableColumn("Selecionar");
+
+
+        tableTime.getColumns().addAll(nomeColumn, ligaColumn, selecionarColumn);
+
+        timeService = ServiceSingleton.getInstance().getTimeService();
+
+        Set<Time> times = timeService.recuperaTimesEMembros();
+        for (Time t : times) {
+            tableTime.getItems().add(new TimeRow(t.getNome(), t.getLiga()));
+        }
     }
 
     @FXML
     public void selecionaTime() {
         seleciona.getParent();
+    }
+
+    public class TimeRow {
+        private final SimpleStringProperty nome, liga;
+
+        private TimeRow(String nome, String liga) {
+            this.nome = new SimpleStringProperty(nome);
+            this.liga = new SimpleStringProperty(liga);
+        }
+
+        public String getNome() {
+            return nome.get();
+        }
+
+        public SimpleStringProperty nomeProperty() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome.set(nome);
+        }
+
+        public String getLiga() {
+            return liga.get();
+        }
+
+        public SimpleStringProperty ligaProperty() {
+            return liga;
+        }
+
+        public void setLiga(String liga) {
+            this.liga.set(liga);
+        }
     }
 }
